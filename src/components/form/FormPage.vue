9f8eb8e03467046b6b957c5c93c9c593a1dd77d9
Sobitov2002 <Sobitov2002@gmail.com>
2024-12-28 18:45:48 +0500

    sasa

--- src/components/form/FormPage.vue
+++ src/components/form/FormPage.vue
@@ -55,9 +55,11 @@ const submit = () => {
 </script>
 
 <template>
-    <form @submit.prevent="submit" method="POST" class="mt-0 mx-auto p-10 bg-white rounded shadow-md">
+    <form @submit.prevent="submit" method="POST" class="mt-0 mx-auto p-10 bg-white rounded shadow-md w-[600px]">
+        <h1 class="text-gray-700 text-[25px] font-bold text-center">Joyingizni band qilish uchun ma'lumotlaringizni
+            yuboring !</h1>
         <div>
-            <label class="block text-gray-700 mb-2" for="fullName">To'liq ism sharifingiz</label>
+            <label class="block mb-2" for="fullName">To'liq ism sharifingiz</label>
             <input required v-model="formData.fullName" id="fullName" type="text"
                 class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Ism,Familya" />
@@ -67,13 +69,13 @@ const submit = () => {
             <input required v-model="formData.phoneNumber" id="phoneNumber" type="number"
                 class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="+998" />
-            <button  class="bg-blue-600 text-white px-[10px] py-[5px] mt-4 rounded-[5px]"
-                type="submit">
+            <button class="bg-blue-600 text-white px-[10px] py-[5px] mt-4 rounded-[5px]" type="submit">
                 Yuborish
             </button>
         </div>
     </form>
 </template>
+
 
 <style scoped>
 
--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -4,7 +4,7 @@ import Price from '@/components/price/Price.vue'
 
 </script>
 <template>
-        <main class="min-h-screen bg-gradient-to-br from-[#e998f7] via-white to-[#aae682]  ">
+        <main class="min-h-screen bg-gradient-to-br from-[#e998f7] via-white to-[#4b9f14]  ">
                 <div class="custom-container max-w-4xl mx-auto p-10 ">
                         <Product />
                         <Price/>     
