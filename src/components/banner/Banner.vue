b88a34c9cc2028f5bc8b3921efa55374b2d92c1a
Sobitov2002 <Sobitov2002@gmail.com>
2024-12-29 09:38:30 +0500

    banner section

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-
 </script>
-<template >
-    <div class="bg-slate-500 h-[100vh]  w-full p-10">
-        <h1 class="text-[100px]">Banner</h1>
+
+<template>
+    <div class="bg-[#fc85f0] h-[90vh] w-full p-10 mybg">
+        <div class="grid grid-cols-3 container">
+            <div>
+            <h1>SAdsnjfndiwrfw</h1></div>
+            <div></div>
+            <div></div>
+        </div>
     </div>
 </template>
 
-<style >
-    
+<style scoped>
+.mybg {
+    background-image: url('https://ik.imagekit.io/vtroph5l9/Product/IMG_0659.png?updatedAt=1735446647811');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+}
 </style>
