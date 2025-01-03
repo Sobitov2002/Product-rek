6c80049d968a0729fa31ca53a0924ff17d407bd9
Sobitov2002 <Sobitov2002@gmail.com>
2025-01-03 22:18:31 +0500

    hh

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -8,7 +8,7 @@ const footer = ref<HTMLElement | null>(null);
 
 <template>
     <div
-        class="bg-gradient-to-r from-[#EFA3A7] via-[#fbdee0] to-[#EFA3A7] md:h-[90vh] h-[70vh] w-full p-10 z-10 relative">
+        class="bg-gradient-to-r from-[#EFA3A7] via-[#fbdee0] to-[#EFA3A7] md:h-[90vh] h-[90vh] w-full p-10 z-10 relative">
         <div class="absolute bottom-0 left-0 w-full h-full z-[-1]">
             <img class=" md:block sm:h-[600px] md:h-[700px] bottom-0 lg:h-[900px] w-auto mx-auto absolute md:left-1/2    transform -translate-x-1/2"
                 src="https://ik.imagekit.io/vtroph5l9/Product/banner.webp?updatedAt=1735485996529" alt="Banner Img">
