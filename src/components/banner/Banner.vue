6ea8a4d12357cdaf4d617aa903357d9945da331a
Sobitov2002 <Sobitov2002@gmail.com>
2025-01-03 22:14:15 +0500

    banner media responsive

--- src/components/banner/Banner.vue
+++ src/components/banner/Banner.vue
@@ -1,13 +1,56 @@
 <script setup lang="ts">
-import Dialog from '@/components/form/Dialog.vue'
+import { ref } from 'vue';
+import Price from '@/components/price/Price.vue'
+const footer = ref<HTMLElement | null>(null);
+
+
 </script>
 
 <template>
     <div
-        class="bg-gradient-to-r from-[#EFA3A7] via-[#FAEAEB] to-[#EFA3A7] md:h-[90vh] h-[80vh] w-full p-10 z-10 relative">
+        class="bg-gradient-to-r from-[#EFA3A7] via-[#fbdee0] to-[#EFA3A7] md:h-[90vh] h-[70vh] w-full p-10 z-10 relative">
         <div class="absolute bottom-0 left-0 w-full h-full z-[-1]">
-            <img class="hidden md:block sm:h-[600px] md:h-[700px] bottom-0 lg:h-[900px] w-auto mx-auto absolute left-1/2 transform -translate-x-1/2"
+            <img class=" md:block sm:h-[600px] md:h-[700px] bottom-0 lg:h-[900px] w-auto mx-auto absolute md:left-1/2    transform -translate-x-1/2"
                 src="https://ik.imagekit.io/vtroph5l9/Product/banner.webp?updatedAt=1735485996529" alt="Banner Img">
+        </div>
+        <div class="container max-w-screen-xl mx-auto px-4">
+            <!-- First section -->
+            <div class="flex justify-between items-center space-x-4">
+                <div class="bg-white p-4 rounded-[10px] shadow-xl">
+                    <h1 class="text-[#BA5A5B] font-bold text-md sm:text-2xl md:text-3xl">5 NOYABR</h1>
+                </div>
+                <div></div>
+                <div class="bg-white p-4 rounded-[10px] shadow-xl">
+                    <h1 class="text-[#BA5A5B] font-bold text-md sm:text-2xl md:text-3xl">6 QOLDI</h1>
+                </div>
+            </div>
+
+            <!-- Next section -->
+            <div class="md:flex md:justify-between justify-end mt-10  md:mt-[150px]">
+                <h1 class="text-[#BA5A5B] font-bold lg:text-[90px] text-end text-[30px] md:text-[70px]">MADINA</h1>
+                <div></div>
+                <h1 class="text-[#BA5A5B] font-bold lg:text-[90px] text-end text-[30px] md:text-[70px]">QODIROVA</h1>
+            </div>
+            <div class="grid md:grid-cols-3  grid-cols-1">
+                <h1 class="text-[#fff] font-bold lg:text-[20px] text-end text-[14px]">Lorem Ipsum is simply dummy text
+                    of the
+                    printing and
+                    typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the </h1>
+                <div></div>
+                <h1 class="text-[#fff] font-bold lg:text-[20px] text-end text-[14px]">QODILorem Ipsum is simply dummy
+                    text of the
+                    printing
+                    and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+                    1500s,</h1>
+            </div>
+
+            <div class="flex items-center md:justify-center justify-end mt-10">
+                <h1
+                    class="cursor-pointer bg-gradient-to-r from-[#BA5A5B] to-[#BA5A5B] md:py-7 py-3 md:px-[180px] px-[60px] text-[20px] text-white rounded-[10px] hover:bg-[#dc7c7e]">
+                    Batafsil
+                </h1>
+
+            </div>
         </div>
 
 
--- src/components/price/Price.vue
+++ src/components/price/Price.vue
@@ -11,7 +11,7 @@ import {ref} from "vue"
 import { priceData } from '../../constant/priceData';
 import Dialog from '@/components/form/Dialog.vue'
 import SubTitle from '@/components/ui/SubTitle.vue'
-
+const footer = ref<HTMLElement | null>(null);
 
 const Data = ref<any>({})
 Data.value = priceData
@@ -20,16 +20,13 @@ Data.value = priceData
 
 </script>
 <template>
-    <div class="md:mt-[130px] mt-[50px]">
-        <SubTitle title="Ta'riflar">
-        </SubTitle>
+    <div ref="footer" class="md:mt-[130px] mt-[50px]">
+        <SubTitle title="Ta'riflar"></SubTitle>
         <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 sm:grid-cols-2 grid-cols-1 relative mt-5">
-
             <Card class="shadow-2xl bg-white backdrop-blur-md rounded-[10px] p-4 mt-6 relative"
                 v-for="(data, index) in priceData" :key="index">
-
-                <div class="relative  ">
-                    <img class=" absolute top-[-50px] left-[50%] transform -translate-x-1/2 h-[140px] w-[140px]  "
+                <div class="relative">
+                    <img class="absolute top-[-50px] left-[50%] transform -translate-x-1/2 h-[140px] w-[140px]"
                         src="../../assets/image/1.svg" alt="">
                 </div>
                 <CardHeader class="mt-[60px] flex items-center">
@@ -45,7 +42,8 @@ Data.value = priceData
                                     viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
-                                </svg></span>
+                                </svg>
+                            </span>
                             {{ item.title }}
                         </li>
                     </ul>
@@ -56,7 +54,6 @@ Data.value = priceData
             </Card>
         </div>
     </div>
-
 </template>
 
 
